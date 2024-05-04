4a702c11a94fb74882726e2fcdf580d8b1d2fd65
Samir-Abasfard <samir.abasfard@gmail.com>
2024-05-04 12:02:10 +0330

    remove test from abasfard admin-dashboard.php linr 28

--- admin/admin-dashboard.php
+++ admin/admin-dashboard.php
@@ -25,7 +25,6 @@ if (!(isset($_SESSION["user_logged"]))) {
     <a href="admin-entekhabvahed.php">مدیریت انتخاب واحد</a>
 
 </div>
-test from abbasfard
 <script src="../assets/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
